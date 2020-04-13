aee1d157ac1f5233d404d03a79a5c21b25a35d74
pranay <pranay.s.chauhan@gmail.com>
2020-04-13 15:57:25 +0530

    added class component with state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 //import Person from './Class-component/Person';
-import Employee from './Functional-component/Employee'
+import Employee from './Functional-component/Employee';
+import Bird from './Class-component/State/Bird'
 function App() {
   return (
     <div className="App">
       {/* <Person/> */}
       <Employee/>
+      <Bird/>
       </div>
   );
 }
